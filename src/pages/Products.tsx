b4c6c60cde84474c512b8194eedaa01958edc50b
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-07-03 00:17:53 +0200

    Actually fix

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Container, Typography, Stack, Button, TextField, Pagination, InputAdornment, Fab } from '@mui/material';
+import { Box, Container, Typography, Stack, Button, TextField, Pagination, InputAdornment, Fab, Chip } from '@mui/material';
 import { Search, Inventory as Package, QrCode, Add, QrCodeScanner } from '@mui/icons-material';
 import { apiClient } from '../lib/api';
 import { useAuth } from '../contexts/AuthContext';
